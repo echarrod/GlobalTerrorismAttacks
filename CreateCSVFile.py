97395acb6b86e9ae34883d3807d2513a88dfb040
Edward Harrod <echarrod@gmail.com>
2017-07-18 18:56:29 +0530

    Tidying up
    Tidying up code

--- CreateCSVFile.py
+++ CreateCSVFile.py
@@ -11,6 +11,7 @@ sys.setdefaultencoding('utf-8') #Required when using python2 with this dataset
 
 newCSVFileName = 'gtd_converted.csv'
 
+
 # Data Preparation Function
 def excel_to_csv():
     filename = config.get('Constants', 'Gtd_filename').strip('""')
@@ -20,4 +21,4 @@ def excel_to_csv():
 
 if __name__ == "__main__":
     excel_to_csv()
-    print "Successfully converted %s to %s." % (config.get('Constants', 'Gtd_filename'), newCSVFileName)
+    print("Successfully converted %s to %s." % (config.get('Constants', 'Gtd_filename'), newCSVFileName)
